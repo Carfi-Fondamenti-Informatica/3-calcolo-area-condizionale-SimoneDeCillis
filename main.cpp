91d387d2468ca24572d6c90e87158fcad5067f83
SimoneDeCillis <116493653+SimoneDeCillis@users.noreply.github.com>
2022-11-21 09:39:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 using namespace std;
 
+
 int main() {
-   return 0;
+    float areat,areaq,arear,a,b;
+    int opzione;
+
+    cin>>a>>b>>opzione;
+    switch (opzione) {
+
+        case 0:
+            areat=(a*b)/2;
+            cout<<areat<<endl;
+            break;
+
+        case 1:
+            areaq=a*a;
+            cout<<areaq<<endl;
+            break;
+
+        case 2:
+            arear=a*b;
+            cout<<arear<<endl;
+            break;
+
+
+            default:
+            cout<<"opzione non valida"<<endl;
+
+    }
+    return 0;
 }
